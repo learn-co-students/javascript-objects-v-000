422960c1ca5dfb8ab52955cadb16bdce01a5323e
sensei100 <jenanton01@gmail.com>
2017-02-19 17:46:01 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {Prince: 'Purple Rain'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
