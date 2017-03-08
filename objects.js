e8f90830bef1f6d1be36c07ed2969d1fd173fd40
lizdenhup <lizdenhup@gmail.com>
2017-03-08 14:54:55 -0600

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  Beyonce: 'Drunk in Love'
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
+
