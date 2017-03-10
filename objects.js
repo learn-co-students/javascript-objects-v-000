2d0ff10ea5d2d98f0a1b4bac7eea46a0632c5da5
Christian Whitesides <whitesidescr@gmail.com>
2017-03-10 16:47:36 -0700

    Done.

--- objects.js
+++ objects.js
@@ -2,8 +2,12 @@ var playlist = {"Carlos Vives" : "Al Filo de Tu Amor"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+
+  return playlist
 }
