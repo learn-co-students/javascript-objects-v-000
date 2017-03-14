46a5fd9d9271bb57d1eccc523227613cc3c7f9ba
Bob Habig <rfhab2@gmail.com>
2017-03-14 13:44:02 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({biggie: 'hypnotize', tupac: 'california love'})
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
