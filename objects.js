fed3843975cbc0ff2ad5a4203dc31d225eae7dc1
joel <joelbitar@protonmail.com>
2017-04-08 17:58:34 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { 'Lady Gaga': 'Eye of the Tiger' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
