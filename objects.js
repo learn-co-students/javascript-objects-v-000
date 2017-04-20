ef47d26c580926601c117115d1dcf2f16b10411c
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-04-19 21:41:41 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {"MBD": "Tamid"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
