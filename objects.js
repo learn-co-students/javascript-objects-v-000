00bace32a0bb6241c74d9046231a9e5543efd8a1
veloblank <veloblank@gmail.com>
2017-06-09 15:16:31 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Bush: "Machinehead"}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
