2f50ed3d782db4aefb4198c66eb75a6f2bff491a
smithwebtek <brad@smithwebtek.com>
2018-03-04 16:56:15 -0500

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,17 @@
 var playlist = { artistName: "song_title" }
+var list = {}
 
 function updatePlaylist(playlist, artistName, song_title) {
-  playlist[artistName] = song_title;
-  return playlist;
+	playlist[artistName] = song_title;
+	return playlist;
+}
+
+function updatelist(list, artistName, song_title) {
+	list[artistName] = song_title;
+	return list;
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
+	delete playlist[artistName]
+	return playlist;
 }
